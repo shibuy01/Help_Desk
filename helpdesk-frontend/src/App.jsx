20047cb1a017d83ffca5f380b60a48506c70be63
Shibu_01 <shibuy0011@gmail.com>
2026-09-10 00:56:17 +0530

    Create Frontend

---
+++ helpdesk-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@base-ui/react'
+import React from 'react'
+import { Card } from './components/ui/card'
+
+export default function App() {
+  return <>
+    <div className='bg-red-500'>App</div>
+    <Button>Click me</Button>
+    <Card/>
+  </>
+}
