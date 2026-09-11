8c021556d246f4f7b4ee23adcc5f7f4d32d992f7
Shibu_01 <shibuy0011@gmail.com>
2026-09-11 08:15:41 +0530

    adding message_bubble

---
+++ helpdesk-frontend/src/components/ui/MessageBubble.jsx
@@ -0,0 +1,60 @@
+// import React from 'react'
+
+// function MessageBubble({key, author, at, children}) {
+
+//     const isMe = author;
+
+//   return (
+//     <div className={`flex ${isMe === "user" ? "justify-end" : "justify-start"}`}>
+        
+//         <div className={`max-w-[65%] rounded-3xl px-3 py-2 shadow-sm ${isMe == "user" ? "bg-slate-900 text-white" : "bg-muted text-foreground"}`}>4
+
+//             <p className='whitespace-pre-wrap break-words leading-relaxed'>{children}</p>
+//             <div className={`mt-1 text-[10px] ${isMe == "user" ? "text-white/70" : "text-muted-foreground"}`}>
+//                 {at}
+//             </div>
+
+//         </div>
+
+//     </div>
+//   )
+// }
+
+// export default MessageBubble
+
+
+import React from "react";
+
+function MessageBubble({ author, at, children }) {
+  const isMe = author === "user";
+
+  return (
+    <div
+      className={`flex ${
+        isMe ? "justify-end" : "justify-start"
+      }`}
+    >
+      <div
+        className={`max-w-[65%] rounded-3xl px-3 py-2 shadow-sm ${
+          isMe
+            ? "bg-slate-900 text-white"
+            : "bg-muted text-foreground"
+        }`}
+      >
+        <p className="whitespace-pre-wrap break-words leading-relaxed">
+          {children}
+        </p>
+
+        <div
+          className={`mt-1 text-[10px] ${
+            isMe ? "text-white/70" : "text-muted-foreground"
+          }`}
+        >
+          {at}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MessageBubble;
--- helpdesk-frontend/src/pages/Chat.jsx
+++ helpdesk-frontend/src/pages/Chat.jsx
@@ -1,6 +1,9 @@
-import { Avatar, AvatarFallback, AvatarImage, Button } from '@base-ui/react'
-import { Plus, Search } from 'lucide-react'
+import { Button } from "@base-ui/react";
+import { Separator, } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { MoreVertical, Plus, Search } from "lucide-react";
 import React from 'react'
+import MessageBubble from "../components/ui/MessageBubble";
 
 
  const CHATS = [
@@ -61,37 +64,63 @@ function Chat() {
     <div className='mx-auto min-h-screen max-w-7xl grid grid-cols-1 md:grid-cols-[300px_minmax(0,1fr)] border-x'>
         <div>
             {/* sidebar */}
-            {/* <aside className='hidden md:flex md:flex-col border-r'></aside> */}
-            <div className='p-3 flex items-center gap-3'>
-                <Button size={'icon'} variant={'outline'} className={'h-8 w-8'}>
-                    <Plus className='h-4 w-4'/>
-                </Button>
-                <div className='relative w-full'>
-                    <input placeholder='Search Chats...' type='text' className='h-9 pl-9 border rounded-xl'/>
-                    <Search className="h-4 w-4 pointer-events-none absolute left-2 top-1/2 -translate-y-1/2 text-muted-foreground"/>
+            <aside className='hidden md:flex md:flex-col border-r'>
+                <div className='p-3 flex items-center gap-3'>
+                    <Button size={'icon'} variant={'outline'} className={'h-8 w-8'}>
+                        <Plus className='h-4 w-4'/>
+                    </Button>
+                    <div className='relative w-full'>
+                        <input placeholder='Search Chats...' type='text' className='h-9 pl-9 border rounded-xl'/>
+                        <Search className="h-4 w-4 pointer-events-none absolute left-2 top-1/2 -translate-y-1/2 text-muted-foreground"/>
+                    </div>
                 </div>
-            </div>
+                <Separator/>
+            </aside>
         </div>
         <div>
             {/* chat area */}
-           <section>
+           <section className="h-full">
                 {/* header */}
-                <div className='flex items-center gap-3 px-4 py-3 border-b'>
-                    <Avatar>
-                        <AvatarImage src='#'/>
-                        <AvatarFallback className={'text-xl'}>
-                            SY
-                        </AvatarFallback>
-                    </Avatar>
-                    <div className='leading-tight'>
-                        <div className='text-sm font-medium'>
-                            Liza Support
+                <div className='flex items-center justify-between gap-3 px-4 py-3 border-b'>
+                    <div className="flex  gap-3">
+                         <div className="h-9 w-9 rounded-full bg-gray-200 text-black flex items-center justify-center text-sm font-medium">
+                            LS
                         </div>
-                        <div className='text-xs text-muted-foreground'>
-                            Online Typing...
+                        <div className='leading-tight'>
+                            <div className='text-sm font-medium'>
+                                Liza Support
+                            </div>
+                            <div className='text-xs text-muted-foreground'>
+                                Online Typing...
+                            </div>
                         </div>
                     </div>
+                    <div>
+                        <Button variant="ghost" size="icon" className={'h-8 w-8'}>
+                            <Search className="h-4 w-4"/>
+                        </Button>
+                        <Button variant="ghost" size="icon" className={'h-8 w-8'}>
+                            <MoreVertical className="h-4 w-4"/>
+                        </Button>
+                    </div>
                 </div>
+
+                {/* Chat Area */}
+                <ScrollArea className="flex-1">
+
+                    <div className="mx-auto max-w-3xl px-6 py-6 space-y-6 ">
+
+                        {
+                            CONVERSATION.map((Chat, index)=>(
+                                <MessageBubble key={Chat.id} author={Chat.author} at={Chat.at}>
+                                    {Chat.text}
+                                </MessageBubble>
+                            ))
+                        }
+
+                    </div>
+
+                </ScrollArea>
            </section>
         </div>
     </div>
